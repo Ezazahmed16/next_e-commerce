9a1351669c5a3868b79c82c8c55dbb550529aa06
Ezaz Ahmed <ezazrahul794@gmail.com>
2024-07-31 05:20:59 +0600

    'logout'

---
+++ src/app/api/invoice/invoice-list/route.js
@@ -0,0 +1,20 @@
+import { PrismaClient } from "@prisma/client";
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
+
+export async function GET(req, res) {
+    try {
+        let headerList = headers();
+        let id = parseInt(headerList.get('id'));
+
+        const prisma = new PrismaClient();
+
+        const result = await prisma.invoices.findMany({
+            where: { user_id: id }
+        })
+
+        return NextResponse.json({ status: 'success', data: result })
+    } catch (error) {
+        return NextResponse.json({ status: 'fail', data: error.message })
+    }
+}
